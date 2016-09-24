885750d58789313834d55425da0c4d10159e74d1
chocofresa <togaopeng@gmail.com>
2016-09-24 20:08:50 +0200

    Speaker--9.24

---
+++ speaker/speaker.ino
@@ -0,0 +1,93 @@
+ #include "pitches.h"
+ 
+void setup() {
+  // iterate over the notes of the melody:
+  pinMode(9,OUTPUT);//for light
+  pinMode(10,OUTPUT);//for light
+  pinMode(11,OUTPUT);//for light
+}
+ 
+void loop() {
+  ColorLight();//Turn on the lights
+  play();//Play the music.
+  delay(300);//Pause for a while.
+}
+ 
+// notes in the melody:
+int melody[] = {
+NOTE_G4,//5  
+NOTE_G4,//5
+NOTE_A4,//6
+NOTE_G4,//5
+NOTE_C5,//1.
+NOTE_B4,//7
+0,
+NOTE_G4,//5
+NOTE_G4,//5
+NOTE_A4,//6
+NOTE_G4,//5
+NOTE_D5,//2.
+NOTE_C5,//1.
+0,
+NOTE_G4,//5
+NOTE_G4,//5
+NOTE_G5,//5.
+NOTE_E5,//3.
+NOTE_C5,//1.
+NOTE_B4,//7
+NOTE_A4,//6
+0,
+NOTE_F5,//4.
+NOTE_F5,//4.
+NOTE_E5,//3.
+NOTE_C5,//1.
+NOTE_D5,//2.
+NOTE_C5,//1.
+0,
+};
+ 
+int noteDurations[] = {
+  8,8,4,4,4,4,
+  4,
+  8,8,4,4,4,4,
+  4,
+  8,8,4,4,4,4,2,
+  8,
+  8,8,4,4,4,2,
+  4,
+};
+ 
+void play()
+{
+    for (int thisNote = 0; thisNote < 29; thisNote++) {
+ 
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(8, melody[thisNote],noteDuration);
+ 
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+}
+ 
+void ColorLight()
+{
+  digitalWrite(9, HIGH);
+  delay(500);
+  digitalWrite(9, LOW);
+  delay(500); 
+  digitalWrite(10, HIGH);
+  delay(500);
+  digitalWrite(10, LOW);
+  delay(500);
+  digitalWrite(11, HIGH);
+  delay(500);
+  digitalWrite(11, LOW);
+  delay(500);
+}
