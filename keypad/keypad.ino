83c19faf6f005d1626df17272c626f7514cbe135
Pruek <pk_lk@hotmail.com>
2016-10-01 18:37:52 +0200

    Add Mario themes

--- keypad/keypad.ino
+++ keypad/keypad.ino
@@ -41,7 +41,106 @@ Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 #define ledpin 13
 int motorPin = 2;
+int melodyPin = 4;
 
+//Mario main theme melody
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//Mario main them tempo
+int tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+//Underworld melody
+int underworld_melody[] = {
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+  NOTE_CS4, NOTE_DS4,
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  0, 0, 0
+};
+//Underwolrd tempo
+int underworld_tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  6, 18, 18, 18,
+  6, 6,
+  6, 6,
+  6, 6,
+  18, 18, 18, 18, 18, 18,
+  10, 10, 10,
+  10, 10, 10,
+  3, 3, 3
+};
 
 void setup()
 {
@@ -81,45 +180,78 @@ void loop()
       // divided by the note type.
       //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
       case '1':
-        tone(4, NOTE_A3,noteDuration);
+        tone(melodyPin, NOTE_A3,noteDuration);
         delay(125);
-        tone(4, NOTE_B3,noteDuration);
+        tone(melodyPin, NOTE_B3,noteDuration);
         delay(125);
-        tone(4, NOTE_C3,noteDuration);
+        tone(melodyPin, NOTE_C3,noteDuration);
         break;
       case '2':
-        tone(4, NOTE_B3,noteDuration);
+        tone(melodyPin, NOTE_B3,noteDuration);
         break;
       case '3':
-        tone(4, NOTE_C4,noteDuration);
+        tone(melodyPin, NOTE_C4,noteDuration);
         break;
       case '4':
-        tone(4, NOTE_D4,noteDuration);
+        tone(melodyPin, NOTE_D4,noteDuration);
         break;
       case '5':
-        tone(4, NOTE_E4,noteDuration);
+        tone(melodyPin, NOTE_E4,noteDuration);
         break;
       case '6':
-        tone(4, NOTE_F4,noteDuration);
+        tone(melodyPin, NOTE_F4,noteDuration);
         break;
       case '7':
-        tone(4, NOTE_G4,noteDuration);
+        tone(melodyPin, NOTE_G4,noteDuration);
         break;
       case '8':
-        tone(4, NOTE_A4,noteDuration);
+        tone(melodyPin, NOTE_A4,noteDuration);
         break;
       case '9':
-        tone(4, NOTE_B4,noteDuration);
+//        tone(melodyPin, NOTE_B4,noteDuration);
+//        int size = sizeof(underworld_melody) / sizeof(int);
+        for (int thisNote = 0; thisNote < 8; thisNote++) {
+     
+          // to calculate the note duration, take one second
+          // divided by the note type.
+          //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+          int noteDuration = 1000 / underworld_tempo[thisNote];
+     
+          tone(melodyPin, underworld_melody[thisNote], noteDuration);
+     
+          // to distinguish the notes, set a minimum time between them.
+          // the note's duration + 30% seems to work well:
+          int pauseBetweenNotes = noteDuration * 1.30;
+          delay(pauseBetweenNotes);
+     
+          // stop the tone playing:
+          tone(melodyPin, 0, noteDuration);
+     
+        }
+
         break;
       case '0':
-        tone(4, NOTE_C5,noteDuration);
+//        tone(melodyPin, NOTE_C5,noteDuration);
+        Serial.println(" 'Mario Theme'");
+//        int size = sizeof(melody) / sizeof(int);
+        for (int thisNote = 0; thisNote < 12; thisNote++) {
+          int noteDuration = 1000 / tempo[thisNote];
+          tone(melodyPin, melody[thisNote], noteDuration);
+          int pauseBetweenNotes = noteDuration * 1.30;
+          delay(pauseBetweenNotes);
+     
+          // stop the tone playing:
+          tone(4, 0, noteDuration);
+    }
+
+
         break;
       case '*':
-        digitalWrite(ledpin, LOW);
+//        digitalWrite(ledpin, LOW);
         analogWrite(motorPin, 200); 
         break;
       case '#':
-        digitalWrite(ledpin, HIGH);
+//        digitalWrite(ledpin, HIGH);
         analogWrite(motorPin, 0);
         break;
       default:
